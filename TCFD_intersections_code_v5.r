9065b06bf5986c8dc5e14d484159b55020025100
atashie <51334454+atashie@users.noreply.github.com>
2024-02-08 09:54:42 -0500

    weekly updates

--- TCFD_intersections_code_v5.r
+++ TCFD_intersections_code_v5.r
@@ -267,14 +267,25 @@ appendedHazardFileLoc =  paste0(customerFolder, 'EQ_CAP_JAN_2024_temp_precip_haz
 waterOnly = FALSE
 
 # 	# EQ Cap - new format
-userName = 'EQ-Capital'	
-customerFolder = paste0('J:\\Cai_data\\TCFD\\locations\\', userName, '_Jan2024b\\')
+userName = 'EQ-CAP-JAN-2024-2-OFFICES'	
+customerFolder = paste0('J:\\Cai_data\\TCFD\\locations\\', userName, '\\')
 
 customerTable = fread(paste0(customerFolder, 'Customer_Hazards_and_Locations-EQCap-JAN2024.csv')) #'HMClause_locations_allCucurbit.csv'
 hazardTable = fread(paste0(customerFolder, 'Hazard_Definitions - Hazard Definitions.csv'))							# 
 relHazScores = fread(paste0(customerFolder, 'Hazard Scores - Hazard_Tables - Hazard Scores.csv'))				
 hazardWeighting = fread(paste0(customerFolder, 'Hazard_Weights - hazard_weights.csv'))				
-appendedHazardFileLoc =  paste0(customerFolder, 'EQ_CAP_JAN_2024_temp_precip_hazards_jan_2.csv')
+appendedHazardFileLoc =  paste0(customerFolder, 'EQ_CAP_JAN_2024_2_OFFICES_temp_precip_hazards_jan_24.csv')
+waterOnly = FALSE
+
+# 	# EQ Cap - new format
+userName = 'EQ-CAP-JAN-2024-2-OFFICES'	
+customerFolder = paste0('J:\\Cai_data\\TCFD\\locations\\', userName, '\\')
+
+customerTable = fread(paste0(customerFolder, 'Customer_Hazards_and_Locations-EQCap-JAN2024.csv')) #'HMClause_locations_allCucurbit.csv'
+hazardTable = fread(paste0(customerFolder, 'Hazard_Definitions - Hazard Definitions.csv'))							# 
+relHazScores = fread(paste0(customerFolder, 'Hazard Scores - Hazard_Tables - Hazard Scores.csv'))				
+hazardWeighting = fread(paste0(customerFolder, 'Hazard_Weights - hazard_weights.csv'))				
+appendedHazardFileLoc =  paste0(customerFolder, 'EQ_CAP_JAN_2024_2_OFFICES_temp_precip_hazards_jan_24.csv')
 waterOnly = FALSE
 
 ########################################################################################################################
@@ -384,7 +395,7 @@ summary(subset(dataOutput, Decade == 2090 & Scenario == "3. High Emissions" & Ha
 summary(subset(dataOutput, Decade == 2090 & Scenario == "2. Middle of the Road" & Hazard == 'Aggregate Climate Score'))
 summary(subset(dataOutput, Decade == 2090 & Scenario == "1. Low Emissions" & Hazard == 'Aggregate Climate Score'))
 par(mfrow = c(3,3))
-thisLoc = 2
+thisLoc = 1
 thisScen = unique(dataOutput$Scenario)[3]
 plot(subset(dataOutput, Scenario == thisScen & Hazard == 'Water Stress' & Location == customerTable$Location[thisLoc])$Percentile)	
 plot(subset(dataOutput, Scenario == thisScen & Hazard == 'Hurricanes' & Location == customerTable$Location[thisLoc])$Percentile)	
--- WaterIndex_v3_interface.r
+++ WaterIndex_v3_interface.r
@@ -13,8 +13,9 @@ clientName = 'Kempen_Nov2023'#	'BeefNW' #'WestHillsFarms' #'AgricolaSanOsvaldo'#
 clientName = 'NuveenLATAM_Dec2023'#	'BeefNW' #'WestHillsFarms' #'AgricolaSanOsvaldo'#	'AgricolaPachacama' #' AgricolaSanTelmo	
 clientName = 'NuveenAus_Dec2023'#	'BeefNW' #'WestHillsFarms' #'AgricolaSanOsvaldo'#	'AgricolaPachacama' #' AgricolaSanTelmo	
 clientName = 'NuveenCali_Jan2024'#	'BeefNW' #'WestHillsFarms' #'AgricolaSanOsvaldo'#	'AgricolaPachacama' #' AgricolaSanTelmo	
+clientName = 'KeithAus_Feb2024'#	'BeefNW' #'WestHillsFarms' #'AgricolaSanOsvaldo'#	'AgricolaPachacama' #' AgricolaSanTelmo	
 
-customerTable = data.table::fread(paste0(customerFolder, clientName, '\\', 'Customer Onboarding Information_NuveenCali_Jan2024.csv'),#Information_NuveenAus_Dec2023.csv'),#Information_Kempen_Nov2023#'Customer Onboarding Information_BNW.csv'),#'Customer Onboarding Information_WestHillsFarms.csv'),#'Customer Onboarding Information_BNW.csv'), 
+customerTable = data.table::fread(paste0(customerFolder, clientName, '\\Customer Onboarding Information_KeithAus_Feb2024.csv'),#Information_NuveenAus_Dec2023.csv'),#Information_Kempen_Nov2023#'Customer Onboarding Information_BNW.csv'),#'Customer Onboarding Information_WestHillsFarms.csv'),#'Customer Onboarding Information_BNW.csv'), 
 	skip = 1) #'Customer_Hazards_and_Locations-Rabobank_grid - Sheet1.csv'
 locationHeader = 'Location (name)'
 
